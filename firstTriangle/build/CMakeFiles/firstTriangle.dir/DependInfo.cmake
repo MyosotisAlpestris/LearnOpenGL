454e4ad07e938157eabe9c101300e7169a45b818
Misuzu <ngc3545106377@qq.com>
2025-11-06 17:23:44 +0800

    triangle

---
+++ firstTriangle/build/CMakeFiles/firstTriangle.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/desktop/Code/Libraries/GLAD/src/glad.c" "CMakeFiles/firstTriangle.dir/D_/desktop/Code/Libraries/GLAD/src/glad.c.obj" "gcc" "CMakeFiles/firstTriangle.dir/D_/desktop/Code/Libraries/GLAD/src/glad.c.obj.d"
+  "D:/desktop/Code/Practice/LearnOpenGL/firstTriangle/src/triangle.cpp" "CMakeFiles/firstTriangle.dir/src/triangle.cpp.obj" "gcc" "CMakeFiles/firstTriangle.dir/src/triangle.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
